e5945abbc2dab8d67a505cc42b34816a41609a81
Brady Wilkinson <bradywilky@gmail.com>
2020-01-22 21:50:26 -0500

    Fix Brady's name in locations where authors are mentioned

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     description='Recursive Feature Elimination on Spark',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Christopher Baker, Matthew Bowman, Brady Wilkerson',
-    author_email='chriscrewbaker@gmail.com',
+    author='Christopher Baker, Matthew Bowman, Brady Wilkinson',
+    author_email='chriscrewbaker@gmail.com', 'bradywilky@gmail.com'
     license='LICENSE.txt',
     packages=['rfespark'],
     install_requires=['pyspark'],
